70b5f6f80460b2724ef9064189de8733535221b6
AlfinQwe <108178631+AlfinQwe@users.noreply.github.com>
2022-10-17 18:51:35 +0700

    Update config.js

--- config.js
+++ config.js
@@ -50,31 +50,31 @@ let wibh = moment.tz('Asia/Jakarta').format('HH')
 // kalo ga punya ketik "-" atau biarin aja biar ada creditsnya :v
 
 global.sig = 'https://instagram.com/mukhammadalfinhidayatulloh' // ig
-global.sgh = 'https://github.com/WH-MODS-BOT/' // github
-global.sgc = 'https://chat.whatsapp.com/FQkbyTEfvES3D363uxMEFOI' // group whatsapp
-global.sdc = 'https://www.discord.com'//discord
-global.snh = 'https://www.youtube.com/channel/UCMx4e8anOq_Olt2nMSv0Cow' // my youtube channel
-global.sfb = 'https://www.facebook.com/' // facebook
-global.syt = 'https://www.youtube.com/' // my youtube channel "kalau ada yt ke 2 tarok link nya disini"
+global.sgh = 'https://github.com/AlfinQwe/' // github
+global.sgc = 'https://chat.whatsapp.com/EG6zJ7J7bz0KgvszJHHg9S' // group whatsapp
+global.sdc = 'https://discord.gg/2GSPuMeY'//discord
+global.snh = 'https://www.youtube.com/channel/UCI3oeINgi5l4hmbe1vgr5Pg' // my youtube channel
+global.sfb = 'https://www.facebook.com/Mukhammad Alfin Hidayatulloh' // facebook
+global.syt = 'https://www.youtube.com/channel/UCI3oeINgi5l4hmbe1vgr5Pg' // my youtube channel "kalau ada yt ke 2 tarok link nya disini"
 
 /*============== PAYMENT ==============*/
-global.pdana = '08XXXXXXXXXX' //dana
-global.povo = '08XXXXXXXXXX' //ovo
-global.pgopay = '08XXXXXXXXXX' //gopay
-global.plinkaja = '08XXXXXXXXXX' //link aja
-global.ppulsa = '08XXXXXXXXXX' //telkomsel
-global.ppulsa2 = '08XXXXXXXXXX' // kalau ada kartu lain isi aja
-global.psaweria = 'https://saweria.com/mrw4h1d'
+global.pdana = '089699565795' //dana
+global.povo = '089699565795' //ovo
+global.pgopay = '089699565795' //gopay
+global.plinkaja = '089699565795' //link aja
+global.ppulsa = '089699565795' //telkomsel
+global.ppulsa2 = '089699565795' // kalau ada kartu lain isi aja
+global.psaweria = 'https://saweria.com/AlfinQwe'
 
 /*============== NOMOR ==============*/
-global.nomorbot = '62XXXXXXXXXXX'
-global.nomorown = '6282127487538'
-global.namebot = 'WH-MODS-BOT-V1'
-global.nameown = 'WH MODS DEV'
+global.nomorbot = '6289699565795'
+global.nomorown = '6289699565795'
+global.namebot = 'MAH MODS DEV'
+global.nameown = 'Mukhammad Alfin Hidayatulloh'
 
 /*============== Github Thanks To ==============*/
-global.namagithub = 'Nama Github Belum Diisi' // nama github lu
-global.linkgithub = 'Link Githu Belum Diisi' // tarok link github lu
+global.namagithub = 'AlfinQwe' // nama github lu
+global.linkgithub = 'https://github.com/AlfinQwe/' // tarok link github lu
 
 // klau kgk ada github kasih tanda '-' aja biar kgk error
 
@@ -85,8 +85,8 @@ global.lolkey = 'e54205a4ca2caa368cc067bb' // PUNYA WAHID  8e66d0934cf741bfd2182
 
 /*============== STAFF ==============*/
 global.owner = [
-  ['6282127487538', 'WH MODS DEV', true],
-  ['-', 'ISI KALAU ADA', true],
+  ['6289699565795', 'Mukhammad Alfin Hidayatulloh', true],
+  ['6289699565795', 'AlfinQwe', true],
   ['-', 'ISI KALAU ADA', true],
   ['-', 'ISI KALAU ADA', true],
   ['-', 'ISI KALAU ADA', true]
