1e6254268be5180a1c950501b50064294bfb6be6
AlfinQwe <108178631+AlfinQwe@users.noreply.github.com>
2022-10-17 19:43:32 +0700

    Update creator.js

--- plugins/creator.js
+++ plugins/creator.js
@@ -11,25 +11,25 @@ let handler  = async (m, { conn, command, args, usedPrefix, DevMode }) => {
 ------- ${nameown} -------
 
 📮 *Note:*
-• Owner tidak menerima save contact
-• Owner berhak blockir tanpa alasan
-• Berbicaralah yang sopan & tidak spam
-• Owner Hanya merespon yang berkaitan dengan BOT
-• No Telp`
+• Alpin ga nerima save kontak
+• Alpin berhak memblockir spamer
+• Berbicaralah yang sopan :)
+• Alpin Hanya merespon hal yang penting
+• boleh telfon asal darurat`
 
 //------------ BIO
 let ppown = await conn.profilePictureUrl(nomorown + '@s.whatsapp.net', 'image').catch(_ => hwaifu[1]) 
 let teksbio = `${htki} *BIODATA* ${htka}
-${htjava} *💌 Nama* : WH-MODS-Dev
-${htjava} *✉️ Nama RL* : HAMBA ALLAH
+${htjava} *💌 Nama* : Mukhammad Alfin Hidayatulloh
+${htjava} *✉️ Nama RL* : Mukhammad Alfin Hidayatulloh
 ${htjava} *♂️ Gender* : Boys
 ${htjava} *🕋 Agama* : Islam
-${htjava} *⏰ Tanggal lahir* : Private 🥶
+${htjava} *⏰ Tanggal lahir* : 9 Mei 2006
 ${htjava} *🎨 Umur* : 18
-${htjava} *🧮 Kelas* : SUDAH TAMAT
+${htjava} *🧮 Kelas* : 10 SMK
 ${htjava} *🧩 Hobby* : MODIFIKASI WHATSAPP "MODDER WHATSAPP"
 ${htjava} *💬 Sifat* : ASIK DAH KALAU DAH KENAL
-${htjava} *🗺️ Tinggal* : Indo, Depok, Jawa Barat
+${htjava} *🗺️ Tinggal* : Indo, Pasuruan, Jawa Timur
 ${htjava} *❤️ Waifu* : -
 
 ${htjava} *📷 ɪɴsᴛᴀɢʀᴀᴍ* : ${sig}
@@ -42,9 +42,9 @@ const sections = [
 	title: `${htjava} OWNER –––––––––·•`,
 	rows: [
 	{title: "📱 • Nomor Owner", rowId: ".owner nomor"},
-	{title: "📱 • Nomor Owner V2", rowId: ".whmods1"},
-	{title: "📱 • Pengembang", rowId: ".whmods2"},
-	{title: "📱 • Creator", rowId: ".whmods3"},
+	{title: "📱 • Nomor Owner V2", rowId: ".owner nomor"},
+	{title: "📱 • Pengembang", rowId: ".owner nomor"},
+	{title: "📱 • Creator", rowId: ".owner nomor"},
 	{title: "🎨 • Biodata", rowId: ".owner bio"},
 	{title: "🌎 • Script", rowId: ".sc"},
 	]
